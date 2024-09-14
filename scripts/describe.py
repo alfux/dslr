59ce1427411c1cc7d396a6d5f832923bce5bf3c9
alfux <alexis.t.fuchs@gmail.com>
2024-09-14 20:54:43 +0200

    describe.py v2

--- scripts/describe.py
+++ scripts/describe.py
@@ -5,11 +5,14 @@ from typing import Generator
 import pandas as pd
 from pandas import DataFrame, Series
 
+from tools import shorten, get_age
+
 
 class Statistics:
     """Stores statistics in fields, based on the given datas"""
-    field_names = ["Count", "Mean", "Var", "Std", "Min", "25%", "50%", "75%",
-                   "Max"]
+    field_names = ["Count", "Mean", "Var", "Unbiased Var", "Std",
+                   "Less Biased Std", "Min", "10%", "20%", "25%", "30%", "40%",
+                   "50%", "60%", "70%", "75%", "80%", "90%", "Max"]
 
     def __init__(self, data: Series) -> None:
         """Initializes each field with its processed value."""
@@ -20,6 +23,14 @@ class Statistics:
         self.fields["25%"] = self.percentile(25)
         self.fields["50%"] = self.percentile(50)
         self.fields["75%"] = self.percentile(75)
+        self.fields["10%"] = self.percentile(10)
+        self.fields["20%"] = self.percentile(20)
+        self.fields["30%"] = self.percentile(30)
+        self.fields["40%"] = self.percentile(40)
+        self.fields["60%"] = self.percentile(60)
+        self.fields["70%"] = self.percentile(70)
+        self.fields["80%"] = self.percentile(80)
+        self.fields["90%"] = self.percentile(90)
         self.fields["Max"] = self.data[-1]
         self.compute_mean_var_std()
 
@@ -40,32 +51,51 @@ class Statistics:
         self.fields["Mean"] /= self.fields["Count"]
         self.fields["Var"] /= self.fields["Count"]
         self.fields["Var"] -= self.fields["Mean"] ** 2
+        self.fields["Unbiased Var"] = self.fields["Var"] * self.fields["Count"]
+        self.fields["Unbiased Var"] /= self.fields["Count"] - 1
         self.fields["Std"] = self.fields["Var"] ** 0.5
+        self.fields["Less Biased Std"] = self.fields["Unbiased Var"] ** 0.5
 
     def quicksort(data: list) -> list:
         """Quicksort sorting algorithm."""
-        if len(data) < 2:
+        if Statistics.is_sorted(data):
             return data
-        pivot = data[len(data) // 2]
-        data1 = [x for x in data if x < pivot]
+        middle = len(data) // 2
+        pivot = data[middle]
+        data1 = [data[i] for i in range(len(data)) if data[i] <= pivot
+                 and i != middle]
         data2 = [x for x in data if x > pivot]
         return (Statistics.quicksort(data1) + [pivot] +
                 Statistics.quicksort(data2))
+
+    def is_sorted(data: list) -> list:
+        """Check if the list is sorted."""
+        if len(data) < 2:
+            return True
+        for i in range(len(data) - 1):
+            if data[i] > data[i + 1]:
+                return False
+        return True
 
 
 def main() -> None:
     """Displays statistics on each courses datas from the argument."""
     try:
-        parser = argparse.ArgumentParser(usage="Describes the data file.")
-        parser.add_argument("data", help="Dataset to be read.")
-        data = pd.read_csv(parser.parse_args().data).select_dtypes(float)
+        parser = argparse.ArgumentParser(usage="describe.py [file] [-w width]")
+        parser.add_argument("data", help="dataset to read")
+        parser.add_argument("-w", help="max name size", default=15, type=int)
+        av = parser.parse_args()
+        data = pd.read_csv(av.data)
+        data["Birthday"] = data["Birthday"].apply(lambda x: get_age(x))
+        data = data.rename(columns={"Birthday": "Age"}).select_dtypes(float)
+        pd.options.display.float_format = lambda x: f"{x:1g}"
 
         def statistics_generator() -> Generator:
             """Generator of Series of Statistics for each columns in data."""
             for x in data.columns:
-                yield Series(Statistics(data[x]).fields, name=x)
+                yield Series(Statistics(data[x]).fields, name=shorten(x, av.w))
 
-        print(DataFrame(statistics_generator()))
+        print(DataFrame(statistics_generator()).transpose())
     except Exception as err:
         print(f"{err.__class__.__name__}: {err}", file=sys.stderr)
 
--- scripts/tools.py
+++ scripts/tools.py
@@ -1,3 +1,4 @@
+from datetime import datetime as date
 from typing import Generator
 
 from pandas import Index
@@ -6,6 +7,10 @@ import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 from matplotlib.widgets import Button
 from matplotlib.backend_bases import KeyEvent
+
+
+def get_age(string: str) -> float:
+    return (date.today() - date.strptime(string, "%Y-%m-%d")).days / 365
 
 
 def close_callback(event: KeyEvent) -> None:
@@ -17,12 +22,13 @@ def close_callback(event: KeyEvent) -> None:
 def shorten(string: str, size: int = 15) -> str:
     """Shortens long names for buttons."""
     if len(string) > size:
-        new_string = ""
-        for c in string:
-            if c.isupper():
-                new_string += c
-        return new_string
-    return string
+        if ' ' in string:
+            new_string = ""
+            for c in string:
+                if c.isupper():
+                    new_string += c
+            return f"{new_string:>.{size}s}"
+    return f"{string:>.{size}s}"
 
 
 def button_gen(fig: Figure, idx: Index) -> Generator:
