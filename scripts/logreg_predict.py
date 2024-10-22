1c68e30dbb1044d8dd3c61ca6c793fdc1343b3a5
alfux <alexis.t.fuchs@gmail.com>
2024-10-22 16:12:24 +0200

    Removed useless fields of data per house.

--- scripts/logreg_predict.py
+++ scripts/logreg_predict.py
@@ -1,38 +1,61 @@
 import argparse
 import sys
+import traceback
 
 import pandas as pd
+from pandas import DataFrame, Series
 import numpy as np
 
 
-def pre_process_data(data: pd.DataFrame) -> pd.DataFrame:
+def pre_process_data(data: DataFrame, weights: DataFrame) -> tuple[list, list]:
     """Pre-processes datas for logistic computations."""
+    for i in range(1, data.shape[1]):
+        data[data.columns[i]] = data[data.columns[i]].apply(
+            lambda x: x if x == x else data_mean(data[data.columns[i]]))
+        raven = reindex(data.drop(["Divination", "History of Magic",
+                                   "Transfiguration", "Flying"], axis=1))
+        wr = [weights.at[i, 'R'] for i in range(len(raven.columns))]
+        slyth = reindex(data.drop(["Transfiguration", "Charms", "Flying",
+                                   "Muggle Studies", "History of Magic"],
+                                  axis=1))
+        ws = [weights['S'][i] for i in range(len(slyth.columns))]
+        gryff = reindex(data.drop(["Divination", "Muggle Studies", "Charms"],
+                                  axis=1))
+        wg = [weights['G'][i] for i in range(len(gryff.columns))]
+        huffl = reindex(data.drop(["Divination", "Muggle Studies",
+                                   "History of Magic", "Transfiguration",
+                                   "Flying"], axis=1))
+        wh = [weights['H'][i] for i in range(len(huffl.columns))]
+    return ([raven, slyth, gryff, huffl], [wr, ws, wg, wh])
+
+
+def reindex(data: DataFrame) -> DataFrame:
+    """Reindexes datas with number."""
     index = {k: i for (k, i) in zip(data.columns, range(data.shape[1]))}
     data = data.rename(index, axis=1)
-    for i in range(1, data.shape[1]):
-        data[i] = data[i].apply(lambda x: x if x == x else data_mean(data[i]))
     return data
 
 
 def data_mean(data: np.array) -> float:
     """Computes mean of data avoiding NaNs"""
     data = [x for x in data if x == x]
+    """Choose a house for a stud based on results."""
     return np.sum(data) / len(data)
 
 
-def sorting_hat(student: pd.Series, weights: pd.Series) -> int:
-    """Choose a house for a student based on results."""
-    expected = student[0]
-    student = student.drop(0)
-    odds = [logistic(student, weights["R"]), logistic(student, weights["S"]),
-            logistic(student, weights["G"]), logistic(student, weights["H"])]
+def sorting_hat(i: int, stud: list[Series], weights: list[DataFrame]) -> int:
+    stud = [stud[0].loc[i], stud[1].loc[i], stud[2].loc[i], stud[3].loc[i]]
+    expected = stud[0][0]
+    for i in range(4):
+        stud[i] = stud[i].drop([0])
+    odds = [logistic(stud[i], weights[i]) for i in range(4)]
     houses = ["Ravenclaw", "Slytherin", "Gryffindor", "Hufflepuff"]
     predicted = houses[odds.index(np.max(odds))]
     print(f"expected: {expected} -> {predicted}")
     return 1 if predicted == expected else 0
 
 
-def logistic(x: pd.Series, weights: pd.Series) -> float:
+def logistic(x: Series, weights: Series) -> float:
     """Computes the logistic function's value in x according to wieghts."""
     return 1 / (1 + np.exp(-np.dot([1, *x], weights)))
 
@@ -46,14 +69,16 @@ def main() -> None:
         parser.add_argument("weights", help="weights from logreg training")
         weights = pd.read_csv(parser.parse_args().weights)
         data = pd.read_csv(parser.parse_args().dataset)
-        data = data.drop(["Index", "First Name", "Last Name",
-                          "Birthday", "Best Hand"], axis="columns")
-        data = pre_process_data(data)
+        data = data.drop(["Index", "First Name", "Last Name", "Arithmancy",
+                          "Birthday", "Best Hand", "Astronomy", "Potions",
+                          "Care of Magical Creatures"], axis="columns")
+        (stud, weights) = pre_process_data(data, weights)
         precision = 0
         for i in range(data.shape[0]):
-            precision += sorting_hat(data.loc[i], weights)
+            precision += sorting_hat(i, stud, weights)
         print(f"The sorting hat was {precision / data.shape[0]:.2%} right")
     except Exception as err:
+        print(traceback.format_exc())
         print(f"{err.__class__.__name__}: {err}", sys.stderr)
 
 
--- scripts/logreg_train.py
+++ scripts/logreg_train.py
@@ -12,14 +12,24 @@ class SortingHatLogreg:
 
     def __init__(self: Self, data: pd.DataFrame, epsilon: float = 1e-2):
         """Starts a logistic regression for each house."""
-        self._data = self._pre_process_data(data)
         self._epsilon = epsilon
+        self._data = self._pre_process_data(data.drop(
+            ["Divination", "History of Magic", "Transfiguration", "Flying"],
+            axis=1))
         self._raven = self._descent(self._observed_ravenclaw)
         print("\rRavenclaw: complete !")
+        self._data = self._pre_process_data(data.drop(
+            ["Transfiguration", "Charms", "Flying", "Muggle Studies",
+             "History of Magic"], axis=1))
         self._slyth = self._descent(self._observed_slytherin)
         print("\rSlytherin: complete !")
+        self._data = self._pre_process_data(data.drop(
+            ["Divination", "Muggle Studies", "Charms"], axis=1))
         self._gryff = self._descent(self._observed_gryffindor)
         print("\rGryffindor: complete !")
+        self._data = self._pre_process_data(data.drop(
+            ["Divination", "Muggle Studies", "History of Magic",
+             "Transfiguration", "Flying"], axis=1))
         self._huffl = self._descent(self._observed_hufflepuff)
         print("\rHufflepuff: complete !")
         self.logreg_coefficients = pd.DataFrame(
@@ -30,7 +40,7 @@ class SortingHatLogreg:
         """Pre-processes datas for logistic regression."""
         index = {k: i for (k, i) in zip(data.columns, range(data.shape[1]))}
         data = data.rename(index, axis=1)
-        self._reduc_coef = [14] * data.shape[1]
+        self._reduc_coef = [1] * data.shape[1]
         for i in range(1, data.shape[1]):
             (mean, reduc) = self._compute_means(data[i])
             data[i] = data[i].apply(lambda x: x / reduc if x == x else mean)
@@ -44,7 +54,7 @@ class SortingHatLogreg:
         for i in range(len(no_nan_data)):
             reduc += self._count_digit(no_nan_data[i])
             mean += no_nan_data[i]
-        reduc = 14 * 10 ** np.round(reduc / len(no_nan_data))
+        reduc = 1 * 10 ** np.round(reduc / len(no_nan_data))
         mean = mean / (len(no_nan_data) * reduc)
         return (mean, reduc)
 
@@ -58,7 +68,7 @@ class SortingHatLogreg:
         return i
 
     def _descent(self: Self, y: Callable) -> np.array:
-        """Returns approximation of point p where log-likelihood is maximal."""
+        """Batch gradient descent to maximize log-likelihood."""
         self._y = y
         p = np.array([0] * len(self._data.columns))
         (nabla, norm) = self._gradient(p)
@@ -67,26 +77,35 @@ class SortingHatLogreg:
             nabla /= norm
             p = p + self._learning_rate(p, nabla) * nabla
             (nabla, norm) = self._gradient(p)
-        for i in range(0, len(p)):
+        for i in range(len(p)):
             p[i] /= self._reduc_coef[i]
-        return p
+        return [p[i] if i < len(p) else 0 for i in range(14)]
+
+    def _stochastic_descent(self: Self, y: Callable) -> np.array:
+        """Stochastic gradient descent to maximize log-likelihood."""
+        self._y = y
+        # p = np.array([0] * len(self._data.columns))
 
     def _learning_rate(self: Self, p: np.array, nabla: np.array) -> float:
         """Computes an approximate of the optimal learning rate."""
-        
+
         def d_dt(t: float) -> float:
             """Derivative of line search log-likelihood."""
             d_dt = 0
             for i in range(self._data.shape[0]):
                 xi = np.array(self._data.loc[i])
                 yi = self._y(xi[0])
-                xi[0] = 1 / 14
+                xi[0] = 1
                 yi_nabla_xi = yi * np.dot(nabla, xi)
                 exposant = yi * np.dot(p, xi) + t * yi_nabla_xi
                 d_dt += yi_nabla_xi / (1 + np.exp(exposant))
             return d_dt / self._data.shape[0]
 
         return self._bissection(d_dt)
+
+    def _stochastic_learning_rate(self: Self, y: Callable) -> float:
+        """Computes an approximate of the optimal stochastic learning rate."""
+        pass
 
     def _bissection(self: Self, f: Callable) -> float:
         """Finds a zero from f by bissection method."""
@@ -112,11 +131,15 @@ class SortingHatLogreg:
         for i in range(self._data.shape[0]):
             xi = [x for x in self._data.loc[i]]
             yi = self._y(xi[0])
-            xi[0] = 1 / 14
+            xi[0] = 1
             p_xi = np.dot(p, xi)
             sum += [yi * xi[j] / (1 + np.exp(yi * p_xi)) for j in range(dim)]
         sum /= self._data.shape[0]
         return (sum, np.linalg.norm(sum))
+
+    def _stochastic_grad(self: Self, p: np.array) -> tuple[np.array, float]:
+        """Computes the gradient of the one-lined-log-likelihood in point p."""
+        pass
 
     def _observed_ravenclaw(self: Self, house: str) -> int:
         """Digital representation of the observed belonging to ravenclaw."""
@@ -134,11 +157,6 @@ class SortingHatLogreg:
         """Digital representation of the observed belonging to hufflepuff."""
         return 1 if house == "Hufflepuff" else -1
 
-    def logistic(p: list, x: list):
-        """Computes the logistic function of parameter p in point x."""
-        x = [n if n == n else 0 for n in x]
-        return 1 / (1 + np.exp(-np.dot(p, np.array(x))))
-
 
 def main() -> None:
     """Trains a logistic regression model to mimic the Sorting Hat"""
@@ -146,8 +164,9 @@ def main() -> None:
         parser = argparse.ArgumentParser(sys.argv[0], f"{sys.argv[0]} [file]")
         parser.add_argument("file", help="csv file containing hogwarts data")
         data = pd.read_csv(parser.parse_args().file)
-        data = data.drop(["Index", "First Name", "Last Name", "Birthday",
-                          "Best Hand"], axis="columns")
+        data = data.drop(["Index", "First Name", "Last Name", "Arithmancy",
+                          "Birthday", "Best Hand", "Astronomy", "Potions",
+                          "Care of Magical Creatures"], axis="columns")
         SortingHat = SortingHatLogreg(data)
         SortingHat.logreg_coefficients.to_csv("./datasets/logreg_coefs.csv",
                                               index=False)
