51cdfa6a3a63f254c2f83ef12c06a8b647229698
alfux <alexis.t.fuchs@gmail.com>
2024-10-24 15:27:03 +0200

    - Added stochastic gradient descent algorithm - Added mini-batch gradient descent algorithm - Added control options - Cleaned code

--- scripts/logreg_train.py
+++ scripts/logreg_train.py
@@ -1,6 +1,5 @@
 import sys
 import argparse
-import traceback
 from typing import Self, Callable
 
 import numpy as np
@@ -10,33 +9,26 @@ import pandas as pd
 class SortingHatLogreg:
     """Computes the sorting hat coefficient by logistic regression."""
 
-    def __init__(self: Self, data: pd.DataFrame, epsilon: float = 1e-2):
+    def __init__(self: Self, data: pd.DataFrame, **kwargs: dict):
         """Starts a logistic regression for each house."""
-        self._epsilon = epsilon
-        # self._pre_process_data(data).to_csv("PLOT.csv")
-        # quit()
-        self._data = self._pre_process_data(data.drop(
-            ["Divination", "History of Magic", "Transfiguration", "Flying"],
-            axis=1))
-        self._raven = self._descent(self._observed_ravenclaw)
-        print("\rRavenclaw: complete !")
-        self._data = self._pre_process_data(data.drop(
-            ["Transfiguration", "Charms", "Flying", "Muggle Studies",
-             "History of Magic"], axis=1))
-        self._slyth = self._descent(self._observed_slytherin)
-        print("\rSlytherin: complete !")
-        self._data = self._pre_process_data(data.drop(
-            ["Divination", "Muggle Studies", "Charms"], axis=1))
-        self._gryff = self._descent(self._observed_gryffindor)
-        print("\rGryffindor: complete !")
-        self._data = self._pre_process_data(data.drop(
-            ["Divination", "Muggle Studies", "History of Magic",
-             "Transfiguration", "Flying"], axis=1))
-        self._huffl = self._descent(self._observed_hufflepuff)
-        print("\rHufflepuff: complete !")
-        self.logreg_coefficients = pd.DataFrame(
-            {"R": self._raven, "S": self._slyth,
-             "G": self._gryff, "H": self._huffl})
+        if "epsilon" in kwargs and kwargs["epsilon"] is not None:
+            self._epsilon = float(kwargs["epsilon"])
+        else:
+            self._epsilon = 1e-2
+        print(f"Maximal gradient norm condition: {self._epsilon}")
+        if "batch" in kwargs and kwargs["batch"] is not None:
+            print("Training with size", end=' ')
+            self._batch = int(kwargs["batch"])
+            print(f"{self._batch} mini-batch gradient descent algorithm:")
+            self._stochastic_algorithm(data)
+        elif "sgd" in kwargs and kwargs["sgd"]:
+            print("Training with stochastic gradient descent algorithm:")
+            self._batch = 1
+            self._stochastic_algorithm(data)
+        else:
+            print("Training with basic (batch) gradient descent algorithm:")
+            self._batch = 0
+            self._batch_algorithm(data)
 
     def _pre_process_data(self: Self, data: pd.DataFrame) -> pd.DataFrame:
         """Pre-processes datas for logistic regression."""
@@ -55,8 +47,7 @@ class SortingHatLogreg:
                     data.at[j, i] = x
             data.reset_index(inplace=True, drop=True)
             self._reduc_coef[i] = reduc
-        # print(data.shape)
-        return data#.rename({i: k for i, k in zip(index.values(), index.keys())}, axis=1)
+        return data
 
     def _compute_by_house(self: Self, df: pd.DataFrame) -> tuple:
         """Computes data's mean and integer digits' mean."""
@@ -78,7 +69,7 @@ class SortingHatLogreg:
         std = {"Ravenclaw": var_r / reduc, "Slytherin": var_s / reduc,
                "Gryffindor": var_g / reduc, "Hufflepuff": var_h / reduc}
         return (mean, std, reduc)
-    
+
     def _std(self: Self, array: np.array, mean: float):
         """Computes unbiased standard deviation."""
         return np.sqrt((np.sum(array ** 2) / len(array) - mean ** 2)
@@ -92,6 +83,56 @@ class SortingHatLogreg:
             number /= 10
             i += 1
         return i
+
+    def _batch_algorithm(self: Self, data: pd.DataFrame) -> None:
+        """Trains the model with the basic (batch) gradient descent."""
+        self._data = self._pre_process_data(data.drop(
+            ["Divination", "History of Magic", "Transfiguration", "Flying"],
+            axis=1))
+        self._rave = self._descent(self._observed_ravenclaw)
+        print("\rRavenclaw: complete !")
+        self._data = self._pre_process_data(data.drop(
+            ["Transfiguration", "Charms", "Flying", "Muggle Studies",
+             "History of Magic"], axis=1))
+        self._slyt = self._descent(self._observed_slytherin)
+        print("\rSlytherin: complete !")
+        self._data = self._pre_process_data(data.drop(
+            ["Divination", "Muggle Studies", "Charms"], axis=1))
+        self._gryf = self._descent(self._observed_gryfindor)
+        print("\rGryffindor: complete !")
+        self._data = self._pre_process_data(data.drop(
+            ["Divination", "Muggle Studies", "History of Magic",
+             "Transfiguration", "Flying"], axis=1))
+        self._huff = self._descent(self._observed_hufflepuff)
+        print("\rHufflepuff: complete !")
+        self.logreg_coef = pd.DataFrame(
+            {"R": self._rave, "S": self._slyt,
+             "G": self._gryf, "H": self._huff})
+
+    def _stochastic_algorithm(self: Self, data: pd.DataFrame) -> None:
+        """Trains the model with the stochastic gradient descent."""
+        self._data = self._pre_process_data(data.drop(
+            ["Divination", "History of Magic", "Transfiguration", "Flying"],
+            axis=1))
+        self._rave = self._stochastic_descent(self._observed_ravenclaw)
+        print("\rRavenclaw: complete !")
+        self._data = self._pre_process_data(data.drop(
+            ["Transfiguration", "Charms", "Flying", "Muggle Studies",
+             "History of Magic"], axis=1))
+        self._slyt = self._stochastic_descent(self._observed_slytherin)
+        print("\rSlytherin: complete !")
+        self._data = self._pre_process_data(data.drop(
+            ["Divination", "Muggle Studies", "Charms"], axis=1))
+        self._gryf = self._stochastic_descent(self._observed_gryfindor)
+        print("\rGryffindor: complete !")
+        self._data = self._pre_process_data(data.drop(
+            ["Divination", "Muggle Studies", "History of Magic",
+             "Transfiguration", "Flying"], axis=1))
+        self._huff = self._stochastic_descent(self._observed_hufflepuff)
+        print("\rHufflepuff: complete !")
+        self.logreg_coef = pd.DataFrame(
+            {"R": self._rave, "S": self._slyt,
+             "G": self._gryf, "H": self._huff})
 
     def _descent(self: Self, y: Callable) -> np.array:
         """Batch gradient descent to maximize log-likelihood."""
@@ -110,11 +151,25 @@ class SortingHatLogreg:
     def _stochastic_descent(self: Self, y: Callable) -> np.array:
         """Stochastic gradient descent to maximize log-likelihood."""
         self._y = y
-        # p = np.array([0] * len(self._data.columns))
+        p = np.array([0] * len(self._data.columns))
+        (nabla, norm) = self._gradient(p)
+        while norm > self._epsilon:
+            print(f"\r{np.clip(self._epsilon / norm, 0, 1):.2%}", end="")
+            self._data = self._data.sample(frac=1)
+            self._data.reset_index(inplace=True, drop=True)
+            for i in range(self._data.shape[0] // self._batch):
+                (nabla, norm) = self._stochastic_grad(p, i)
+                if norm > 1:
+                    nabla /= norm
+                p = p + self._stochastic_learning_rate(p, nabla, i) * nabla
+            (nabla, norm) = self._gradient(p)
+        for i in range(len(p)):
+            p[i] /= self._reduc_coef[i]
+        return [p[i] if i < len(p) else 0 for i in range(14)]
 
     def _learning_rate(self: Self, p: np.array, nabla: np.array) -> float:
         """Computes an approximate of the optimal learning rate."""
-        
+
         def d_dt(t: float) -> float:
             """Derivative of line search log-likelihood."""
             d_dt = 0
@@ -129,9 +184,23 @@ class SortingHatLogreg:
 
         return self._bissection(d_dt)
 
-    def _stochastic_learning_rate(self: Self, y: Callable) -> float:
+    def _stochastic_learning_rate(
+            self: Self, p: np.array, nabla: np.array, line: int) -> float:
         """Computes an approximate of the optimal stochastic learning rate."""
-        pass
+
+        def d_dt(t: float) -> float:
+            """Derivative of line search stochastic log-likelihood."""
+            d_dt = 0
+            for i in range(line, line + self._batch):
+                xi = np.array(self._data.loc[i])
+                yi = self._y(xi[0])
+                xi[0] = 1
+                yi_nabla_xi = yi * np.dot(nabla, xi)
+                exposant = yi * np.dot(p, xi) + t * yi_nabla_xi
+                d_dt += yi_nabla_xi / (1 + np.exp(exposant))
+            return d_dt / self._data.shape[0]
+
+        return self._bissection(d_dt)
 
     def _bissection(self: Self, f: Callable) -> float:
         """Finds a zero from f by bissection method."""
@@ -148,22 +217,31 @@ class SortingHatLogreg:
                 (b, fb) = (c, fc)
         return a if np.abs(fa) < np.abs(fb) else b
 
-    def _gradient(self: Self, p: np.array) -> tuple[np.array, float]:
+    def _gradient(self: Self, p: np.array) -> tuple:
         """Computes the gradient of the log-likelihood in point p."""
         dim = self._data.shape[1]
-        sum = np.array([0.0] * dim)
+        grad = np.array([0.0] * dim)
         for i in range(self._data.shape[0]):
-            xi = [x for x in self._data.loc[i]]
+            xi = np.array(self._data.loc[i])
             yi = self._y(xi[0])
             xi[0] = 1
             p_xi = np.dot(p, xi)
-            sum += [yi * xi[j] / (1 + np.exp(yi * p_xi)) for j in range(dim)]
-        sum /= self._data.shape[0]
-        return (sum, np.linalg.norm(sum))
+            grad += [yi * xi[j] / (1 + np.exp(yi * p_xi)) for j in range(dim)]
+        grad /= self._data.shape[0]
+        return (grad, np.linalg.norm(grad))
 
-    def _stochastic_grad(self: Self, p: np.array) -> tuple[np.array, float]:
+    def _stochastic_grad(self: Self, p: np.array, line: int) -> tuple:
         """Computes the gradient of the one-lined-log-likelihood in point p."""
-        pass
+        dim = self._data.shape[1]
+        grad = np.array([0.0] * dim)
+        for i in range(line, line + self._batch):
+            xi = np.array(self._data.loc[i])
+            yi = self._y(xi[0])
+            xi[0] = 1
+            p_xi = np.dot(p, xi)
+            grad += [yi * xi[j] / (1 + np.exp(yi * p_xi)) for j in range(dim)]
+        grad /= self._batch
+        return (np.array(grad), np.linalg.norm(grad))
 
     def _observed_ravenclaw(self: Self, house: str) -> int:
         """Digital representation of the observed belonging to ravenclaw."""
@@ -173,7 +251,7 @@ class SortingHatLogreg:
         """Digital representation of the observed belonging to slytherin."""
         return 1 if house == "Slytherin" else -1
 
-    def _observed_gryffindor(self: Self, house: str) -> int:
+    def _observed_gryfindor(self: Self, house: str) -> int:
         """Digital representation of the observed belonging to gryffindor."""
         return 1 if house == "Gryffindor" else -1
 
@@ -187,21 +265,22 @@ def main() -> None:
     try:
         parser = argparse.ArgumentParser(
             sys.argv[0], f"{sys.argv[0]} [file] [-s] [-m batch]")
-        parser.add_argument("-s", "--stochastic-gd", action="store_const",
-                            help="use stochastic gradient descent")
+        parser.add_argument("file", help="csv file containing hogwarts datas")
+        parser.add_argument("-s", "--stochastic-gd", action="store_true",
+                            help="use stochastic gradient descent", )
         parser.add_argument("-m", "--mini-batch-gd",
                             help="use mini-batch gradient descent")
-        parser.add_argument("file", help="csv file containing hogwarts datas")
+        parser.add_argument("-e", "--epsilon", help="sets epsilon precision")
         data = pd.read_csv(parser.parse_args().file)
-        # SortingHatLogreg(data.drop(["Index", "First Name", "Last Name", "Birthday", "Best Hand"], axis=1))
         data = data.drop(["Index", "First Name", "Last Name", "Arithmancy",
                           "Birthday", "Best Hand", "Astronomy", "Potions",
                           "Care of Magical Creatures"], axis=1)
-        SortingHat = SortingHatLogreg(data)
-        SortingHat.logreg_coefficients.to_csv("./datasets/logreg_coefs.csv",
-                                              index=False)
+        SortingHat = SortingHatLogreg(
+            data, epsilon=parser.parse_args().epsilon,
+            batch=parser.parse_args().mini_batch_gd,
+            sgd=parser.parse_args().stochastic_gd)
+        SortingHat.logreg_coef.to_csv("./logreg_coef.csv", index=False)
     except Exception as err:
-        print(traceback.format_exc())
         print(f"{err.__class__.__name__}: {err}", sys.stderr)
 
 
